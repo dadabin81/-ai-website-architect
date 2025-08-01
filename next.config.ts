8f1bc13be23af7b19fce06708db45d007d356127
bin data <databin81@gmail.com>
2025-08-01 00:10:53 +0000

    2025-07-31 23:58:53.029 [info] [main] Log level: Info 2025-07-31 23:58:5

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Your Next.js configuration options go here.
-  // For example, to add a new environment variable:
-  // env: {
-  //   MY_CUSTOM_KEY: 'my-value',
-  // },
-
-  // By default, Next.js will automatically inline font CSS at build time.
-  // This can be disabled if you prefer to load fonts manually.
-  // optimizeFonts: false,
 };
 
 export default nextConfig;
